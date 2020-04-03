c7f3ae63faae96b5d4734135e0d912456b032774
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2020-04-03 04:45:28 +0300

    init by vs code

---
+++ component---src-pages-404-js-bdda0cf536b78853ea34.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{270:function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),s=n(263);t.default=function(e){var t=e.location;return o.a.createElement(s.a,{location:t},o.a.createElement("h1",null,"NOT FOUND"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bdda0cf536b78853ea34.js.map
